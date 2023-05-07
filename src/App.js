f052507b00fd32c5bd0f78fd3e696aa450402607
Thierry <thierryvm@hotmail.com>
2023-05-07 20:47:55 +0200

    Modification flex

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className="App text-center bg-cyan-400 text-4xl text-slate-600 font-sans hover:font-serif font-semibold">
+    <div className="App flex justify-center bg-cyan-400 text-4xl text-slate-600 font-sans hover:font-serif font-semibold w-screen h-40 p-12">
       Burger House
     </div>
   )
