42f6efae96ca82401a25f8ccfb2364f121d77854
thierryvm <thierryvm@hotmail.com>
2023-05-07 17:22:17 +0200

    Clean App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-6xl font-bold underline bg-slate-300">
-          Hello world!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button class="bg-sky-500 hover:bg-sky-700 ...">
-          Save changes
-        </button>
-      </header>
+    <div className="App text-center bg-cyan-400 text-4xl text-slate-600">
+      Hello World!
     </div>
-  );
+  )
 }
 
-export default App;
+
+
