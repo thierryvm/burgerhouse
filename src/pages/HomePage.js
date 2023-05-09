9ad729bffcd790e41ff84259ac75bc7331d744c8
Thierry <thierryvm@hotmail.com>
2023-05-10 00:13:15 +0200

    Modification de style

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,8 @@ import { BeakerIcon } from '@heroicons/react/24/solid'
 
 export default function HomePage() {
   return (
-    <div>
-        <h1 className='flex items-center gap-2 justify-center '><BeakerIcon className="h-10 w-10 inline" /><span>world from HomePage !</span> 
+    <div className='p-10'>
+        <h1 className='flex items-center gap-2 justify-center '><BeakerIcon className="h-10 w-10 inline" /><span  className='text-3xl text-green-500 '>world from HomePage !</span> 
         </h1>
     </div>
   )
